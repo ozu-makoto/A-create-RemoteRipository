b971ba89bd74e8182fd094a9c440d5e7be8677c6
ozu <k16002kk@aitech.ac.jp>
2018-10-16 10:37:53 +0900

    A-コメント文の追加

--- view.php
+++ view.php
@@ -17,7 +17,7 @@ try {
   $pdo = new PDO($dsn, $db_user, $db_pass);
   $pdo->setAttribute(PDO::ATTR_ERRMODE,
                      PDO::ERRMODE_EXCEPTION);
-  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);//おはよう
   print "接続しました...<br>";
 } catch(PDOException $Exception) {
   die('接続エラー:' . $Exception->getMessage());
